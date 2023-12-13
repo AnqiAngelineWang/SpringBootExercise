3307dcda9a29dd12af43e9619de6193a1899e236
AnqiAngelineWang <32228475+AnqiAngelineWang@users.noreply.github.com>
2023-12-12 20:43:13 -0500

    Update GeoLocResponse.java

--- src/main/java/com/userregistrationsystem/model/GeoLocResponse.java
+++ src/main/java/com/userregistrationsystem/model/GeoLocResponse.java
@@ -2,11 +2,107 @@ package com.userregistrationsystem.model;
 
 public class GeoLocResponse {     // API server response POSTMAN to me
     String city;
-  //  String uuid;
-  //  String message;
     String country;
-
     String query;
+    String status;
+    String countryCode;
+    String region;
+    String regionName;
+    String zip;
+    String lat;
+    String lon;
+    String timezone;
+    String isp;
+    String org;
+    String as;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public String getIsp() {
+        return isp;
+    }
+
+    public void setIsp(String isp) {
+        this.isp = isp;
+    }
+
+    public String getOrg() {
+        return org;
+    }
+
+    public void setOrg(String org) {
+        this.org = org;
+    }
+
+    public String getAs() {
+        return as;
+    }
+
+    public void setAs(String as) {
+        this.as = as;
+    }
 
     public String getQuery() {
         return query;
@@ -32,21 +128,7 @@ public class GeoLocResponse {     // API server response POSTMAN to me
         this.city = city;
     }
 
-    // public String getUuid() {
-    //     return uuid;
-    // }
 
-    // public void setUuid(String uuid) {
-    //     this.uuid = uuid;
-    // }
-
-    // public String getMessage() {
-    //     return message;
-    // }
-
-    // public void setMessage(String message) {
-    //     this.message = message;
-    // }
 }
 
 
